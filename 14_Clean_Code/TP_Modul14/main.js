1848ea4777efafc143a77031b23114e64408f7bc
ahmadUffi <ahmaduffi45@gmail.com>
2025-05-28 22:08:21 +0700

    submit yang belum

---
+++ 14_Clean_Code/TP_Modul14/main.js
@@ -0,0 +1,17 @@
+// main.js
+// Menjalankan program utama untuk mengetes inheritance, encapsulation dan polymorphism
+
+import Manager from "./Manager.mjs";
+
+try {
+  const manager1 = new Manager(
+    "Ahmad Uffi",
+    40,
+    "Project Manager",
+    10000000,
+    2500000
+  );
+  console.log(manager1.introduce()); // Output dengan total salary
+} catch (error) {
+  console.error(error.message);
+}
