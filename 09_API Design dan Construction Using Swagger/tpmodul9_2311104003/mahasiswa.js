73f0130937b64f330b09586f2a14968cd7f31c68
ahmadUffi <ahmaduffi45@gmail.com>
2025-06-05 23:21:29 +0700

    final

---
+++ 09_API Design dan Construction Using Swagger/tpmodul9_2311104003/mahasiswa.js
@@ -0,0 +1,7 @@
+const mahasiswa = [
+  { nama: "Ahmad Uffi", nim: "2311104015" },
+  { nama: "Sinta Alimah", nim: "23111040100" },
+  { nama: "Banyu", nim: "23111040101" },
+];
+
+module.exports = mahasiswa;
